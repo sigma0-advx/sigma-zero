e49c01eb356d2cab953eacb4284d54295da53e37
francescovillani <francesco.villani99@gmail.com>
2025-07-04 15:13:41 +0000

    enabled early stopping through epsilon_budget parameter

--- sigma_zero_attack.py
+++ sigma_zero_attack.py
@@ -29,27 +29,36 @@ def sigma_zero(model: nn.Module,
     optimizer = torch.optim.Adam([delta], lr=lr)
     scheduler = lr_scheduler.CosineAnnealingLR(optimizer, T_max=steps, eta_min=lr / 10)
     best_delta = delta.clone()
+    query_mask = torch.full((batch_size,), True, device=device)
     best_l0 = torch.full((batch_size,), max_size, device=device)
+    is_adv_below_eps = torch.full((batch_size,), False, device=device)
     th = torch.ones(size=inputs.shape, device=device) * threshold
-
+    
     for i in range(steps):
         optimizer.zero_grad()
 
-        adv_inputs = inputs + delta  
+        active_delta = delta[query_mask].clone().detach().requires_grad_(True)
+        active_inputs = inputs[query_mask]
+        active_labels = labels[query_mask]  
+        
+        adv_inputs = active_inputs + active_delta
 
         # compute loss
         logits = model(adv_inputs)
-        dl_loss = difference_of_logits(logits, labels).clip(0) 
-        l0_approx = l0_approximation(delta.flatten(1), sigma)
-        l0_approx_normalized = l0_approx / delta.data.flatten(1).shape[1]
+        dl_loss = difference_of_logits(logits, active_labels).clip(0)
+        l0_approx = l0_approximation(active_delta.flatten(1), sigma)
+        l0_approx_normalized = l0_approx / active_delta.data.flatten(1).shape[1]
+
         # keep best solutions
         predicted_classes = (logits).argmax(1)
-        true_l0 = delta.data.flatten(1).ne(0).sum(dim=1)
-        is_not_adv = predicted_classes == labels
-        is_smaller = true_l0 < best_l0
+        true_l0 = active_delta.data.flatten(1).ne(0).sum(dim=1)
+        is_not_adv = predicted_classes == active_labels
+        is_smaller = true_l0 < best_l0[query_mask]
         is_both = ~is_not_adv & is_smaller
-        best_l0 = torch.where(is_both, true_l0.detach(), best_l0)
-        best_delta = torch.where(batch_view(is_both), delta.data.clone().detach(), best_delta)
+        best_l0[query_mask] = torch.where(is_both, true_l0.detach(), best_l0[query_mask])
+        best_delta[query_mask] = torch.where(batch_view(is_both), active_delta.data.clone().detach(), best_delta[query_mask]) 
+        is_adv_below_eps = best_l0 <= epsilon_budget if epsilon_budget is not None else is_adv_below_eps 
+    
         # update step
         adv_loss = (is_not_adv + dl_loss + l0_approx_normalized).mean()
 
@@ -61,6 +70,11 @@ def sigma_zero(model: nn.Module,
 
         adv_loss.backward()
 
+        if delta.grad is None:
+            delta.grad = torch.zeros_like(delta, device=device)
+        # Copy gradients from active_delta.grad to delta.grad at the masked positions
+        delta.grad[query_mask] += active_delta.grad
+ 
         delta.grad.data = normalize(delta.grad.data)
         optimizer.step()
         scheduler.step()
@@ -69,10 +83,17 @@ def sigma_zero(model: nn.Module,
             # enforce box constraints
             clamp(delta.data)
             # dynamic thresholding step
-            (th)[is_not_adv, :, :, :] -= t * scheduler.get_last_lr()[0]
-            (th)[~is_not_adv, :, :, :] += t * scheduler.get_last_lr()[0]
+            th_active = th[query_mask]
+        
+            th_active[is_not_adv, :, :, :] -= t * scheduler.get_last_lr()[0]
+            th_active[~is_not_adv, :, :, :] += t * scheduler.get_last_lr()[0]
+            th[query_mask] = th_active
             th.clamp_(0, 1)
             # filter components
             delta.data[delta.data.abs() < th] = 0
+            # update active set
+            query_mask[is_adv_below_eps] = False
+            if not any(query_mask):
+                break
 
     return (inputs + best_delta)
